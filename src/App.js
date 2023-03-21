926fec41edf44ca6897821298bb89942dd42f62f
Styrlingjohn <styrling_morris@yahoo.com>
2023-03-21 17:32:04 -0600

    added starter code and a couple of components with minimal css

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import HelloDiv from "./components/Site"
+import './App.css';
+
+
+
+function App() {
+    return <HelloDiv />;
+    }
+
+    export default App;
---
+++ src/components/Site.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Nav from "./Nav";
+
+function HelloDiv() {
+  return (
+    <div>
+      <Nav/>
+      <h2></h2>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  );
+}
+
+export default HelloDiv;
