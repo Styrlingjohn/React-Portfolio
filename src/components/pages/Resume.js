ce365322674058c1fa87432fd919eb3fb02987d3
Styrlingjohn <styrling_morris@yahoo.com>
2023-03-23 20:45:17 -0600

    added resume download and resume content

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,22 +1,49 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 export default function Resume() {
+    // Function will execute on click of button
+    const onButtonClick = () => {
+      // using Java Script method to get PDF file
+      fetch('Resume.pdf').then(response => {
+          response.blob().then(blob => {
+              // Creating new object of PDF file
+              const fileURL = window.URL.createObjectURL(blob);
+              // Setting various property values
+              let alink = document.createElement('a');
+              alink.href = fileURL;
+              alink.download = 'Resume.pdf';
+              alink.click();
+          })
+        })
+      }
   return (
 
-    <div>
-      <h1>Resume Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
-      </p>
-    </div>
+    <Container>
+      <h1>Portfolio</h1>
+
+      <button onClick={onButtonClick}>Downloadable Resume</button>
+
+      <Row className='rows'>
+        <Col>
+          <li>JavaScript</li>
+          <li>React</li>
+          <li>MongoDB</li>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>Insomnia</li>
+        </Col>
+        <Col>
+          <li>MySQL</li>
+          <li>MVC</li>
+          <li>NodeJS</li>
+          <li>Express</li>
+          <li>Object-oriented programming</li>
+        </Col>
+      </Row>
+    </Container>
   );
-}
+  };
