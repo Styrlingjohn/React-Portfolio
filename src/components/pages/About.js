fafd69727b1f0dfb353c3e3d5f600bae9562ac20
Styrlingjohn <styrling_morris@yahoo.com>
2023-04-25 16:50:30 -0600

    fixed About me page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -9,10 +9,9 @@ import React from 'react';
   }
   return (
     <div>
-      <h1>About Me</h1>
+      <h1>About Styrling Morris</h1>
       <p className='About'>
-    Currently enrolled in a full-stack computer programming bootcamp through the University of Utah.
-    I am scheduled to complete the course on April 17th. I have learned many skills during my journey but 
+    Recently completed a full-stack computer programming bootcamp through the University of Utah. I learned to utilize many skills during my journey but 
     I am excited to further my knowledge by entering the workforce. I love to be social and interact with people.
     Me and my wife have been married for almost 4 years and we love to travel. Some notable places we have been is 
     Athens, Greece; Santorini, Greece; Cabo, Mexico; Roaton, Honduras; Maui, Hawaii. I am a person that has a lot to 
